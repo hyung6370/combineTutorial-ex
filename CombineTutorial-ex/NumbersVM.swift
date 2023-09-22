42df51980cd38ed5760c8ab7c4137aae224fb290
emptyhead <korea6370@yu.ac.kr>
2023-09-22 14:18:04 +0900

    2023/9/22 update

--- CombineTutorial-ex/NumbersVM.swift
+++ CombineTutorial-ex/NumbersVM.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import Combine
+import UIKit
+
+class NumbersVM: ObservableObject {
+    
+    var subscriptions = Set<AnyCancellable>()
+    
+    // Input - ViewModel로 들어오는 녀석
+    @Published var number1: String = ""
+    @Published var number2: String = ""
+    @Published var number3: String = ""
+    @Published var number4: String = ""
+    
+    
+    // output - ViewModel에서 나가는 녀석
+    @Published var resultValue: String = ""
+    
+    lazy var resultPublisher: AnyPublisher<String, Never> = Publishers
+        .CombineLatest4($number1, $number2, $number3, $number4)
+            .map { textValue1, textValue2, textValue3, textValue4 -> Int in
+                return textValue1.getNumber() + textValue2.getNumber() + textValue3.getNumber() + textValue4.getNumber()
+            }
+            .map { "\($0)"}
+            .eraseToAnyPublisher()
+    
+    init() {
+        print(#fileID, #function, #line, "- ")
+        
+        resultPublisher
+            .assign(to: \.resultValue, on: self)
+            .store(in: &subscriptions)
+        
+//        Publishers
+//            .CombineLatest4($number1, $number2, $number3, $number4)
+//            .map { textValue1, textValue2, textValue3, textValue4 -> Int in
+//                return textValue1.getNumber() + textValue2.getNumber() + textValue3.getNumber() + textValue4.getNumber()
+//            }
+//            .map { "\($0)"}
+//            .assign(to: \.resultValue, on: self)
+////            .sink(receiveValue: { value in
+////                print(#fileID, #function, #line, "- value: \(value)")
+////                self.resultValue = value
+////            })
+//            .store(in: &subscriptions)
+    }
+}
--- CombineTutorial-ex/ViewController.swift
+++ CombineTutorial-ex/ViewController.swift
@@ -6,12 +6,45 @@
 //
 
 import UIKit
+import CombineCocoa
+import Combine
 
 class ViewController: UIViewController {
-
+    
+    var subscriptions = Set<AnyCancellable>()
+    
+    @IBOutlet weak var navToNumbersBtn: UIButton!
+    @IBOutlet weak var navToNumbersSwiftUIBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navToNumbersBtn
+            .tapPublisher
+            .sink(receiveValue: {
+                print(#fileID, #function, #line, "- ")
+                
+                #warning("TODO : - numbers로 화면 이동")
+                let numbersVC = NumbersViewController.instantiate("Numbers")
+                self.navigationController?.pushViewController(numbersVC, animated: true)
+            })
+            .store(in: &subscriptions)
+        
+        navToNumbersSwiftUIBtn
+            .tapPublisher
+            .sink(receiveValue: {
+                print(#fileID, #function, #line, "- ")
+                
+                #warning("TODO : - numbers로 화면 이동")
+//                let numbersVC = NumbersSwiftUiViewContainerVC()
+//                let numbersVC = SwiftUiContainerVC(swiftUiView: NumbersView())
+                let numbersVC = NumbersView().getContainerVC()
+//                let myVC = MyView().getContainerVC()
+                self.navigationController?.pushViewController(numbersVC, animated: true)
+//                self.navigationController?.pushViewController(myVC, animated: true)
+            })
+            .store(in: &subscriptions)
+        
     }
 
 
